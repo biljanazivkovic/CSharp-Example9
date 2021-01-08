35ecf141bc0d26d21303ea2cca299ab4227a812d
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:16:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ex9
+{
+    class Program
+    {
+        /* Methods */
+
+        static void MyMethod()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        static void MySecondMethod(string fname, int age)
+        {
+            Console.WriteLine(fname + " Refsnes" + " is " + age);
+        }
+
+        static int MyThirdMethod(int x)
+        {
+            return x + 5;
+        }
+
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+        static double PlusMethod(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+            MySecondMethod("Liam", 15);
+            MySecondMethod("Jenny", 8);
+            MySecondMethod("Anja", 21);
+            Console.WriteLine(MyThirdMethod(5));
+            Console.WriteLine("Int: " + PlusMethod(8, 3));
+            Console.WriteLine("Double: " + PlusMethod(5.2, 6.9));
+        }
+    }
+}
